f32473cc774bfc4d9bf5a0c20cc3110420aed6a6
Yu-Chen Hsu <every2ever@gmail.com>
2017-10-11 03:38:00 -0700

    implement compiling function for markdown text

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-new Vue({
+var app = new Vue({
 	el: '#editor',
 	data: {
 		text: "# hello",
 		compiled: "<h1>hello<h1>"
+	},
+	watch: {
+		// text() {
+		// 	this.compiled = marked(this.text, { sanitize: true });
+		// }
+		text: _.debounce(function (val){
+			app.compiled = marked(val, { sanitize: true });
+		}, 300)
 	}
 });
